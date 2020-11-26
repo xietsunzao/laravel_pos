cfa3973d7d9330e11a16d37bb844a840377ed316
xietsunzao <lordshinjo22@gmail.com>
2020-11-26 13:40:32 +0700

    initial commit

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    public function index()
+    {
+        
+    }
+}   
