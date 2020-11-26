f9424df36583b9c06333dfd9aed9a74c41efa8e8
xietsunzao <lordshinjo22@gmail.com>
2020-11-26 13:53:51 +0700

    Added auth vue laravel

---
+++ database/migrations/2020_11_26_064158_create_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_kategori', function (Blueprint $table) {
+            $table->tinyIncrements('id_kategori');
+            $table->string('nama_kategori', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_kategori');
+    }
+}
